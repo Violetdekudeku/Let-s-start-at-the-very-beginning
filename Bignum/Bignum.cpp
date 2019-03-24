ea35edff3dc759b403c9044ca3aaea4ae78c9c27
Violetdekudeku <15617164607@163.com>
2019-03-24 12:34:31 +0800

    initial commit

--- Bignum/Bignum.cpp
+++ Bignum/Bignum.cpp
@@ -19,6 +19,9 @@ class Bignum
     friend ostream& operator<<(ostream&, Bignum&);
 
     friend Bignum operator+(const Bignum& a,const Bignum& b);
+    friend Bignum operator-(const Bignum& a,const Bignum& b);
+    friend bool operator>(const Bignum& a,const Bignum&b);
+    friend bool operator<(const Bignum& a,const Bignum&b);
     private:
     vector <int>a;
     int symbol;
@@ -209,16 +212,150 @@ Bignum operator+(const Bignum& a,const Bignum& b)
     return c;
 }
 
+Bignum operator-(const Bignum& a,const Bignum& b)
+{
+    Bignum c;int m,n;int i,j;int h,t;
+    while(!c.a.empty())
+        c.a.pop_back();
+    m=a.a.size();n=b.a.size();
+    if(a>b)
+    {
+        c.symbol=1;
+        Bignum d(a);
+        for(i=m-1,j=n-1;i>-1&&j>-1;i--,j--)
+        {
+            if(d.a.at(i)>=b.a.at(j))
+                c.a.push_back(d.a.at(i)-b.a.at(j));
+            else
+            {
+                c.a.push_back(d.a.at(i)+10-b.a.at(j));
+                h=i-1;
+                while(d.a.at(h)==0)
+                {
+                    d.a.at(h)=9;
+                    h--;
+                }
+                d.a.at(h)--;
+            }
+        }
+        while(i>-1)
+        {
+            c.a.push_back(d.a.at(i));
+            i--;
+        }
+    }
+    else
+    {
+        c.symbol=0;
+        Bignum d(b);
+        for(i=m-1,j=n-1;i>-1&&j>-1;i--,j--)
+        {
+            if(d.a.at(j)>=a.a.at(i))
+                c.a.push_back(d.a.at(j)-a.a.at(i));
+            else
+            {
+                c.a.push_back(d.a.at(j)+10-a.a.at(i));
+                h=j-1;
+                while(d.a.at(h)==0)
+                {
+                    d.a.at(h)=9;
+                    h--;
+                }
+                d.a.at(h)--;
+            }
+            
+        }
+        while(j>-1)
+        {
+            c.a.push_back(d.a.at(j));
+            j--;
+        }
+    }
+    h=c.a.size();
+    for(i=0;i<h/2;i++)
+    {
+        t=c.a.at(i);c.a.at(i)=c.a.at(h-1-i);c.a.at(h-1-i)=t;
+    }
+    return c;
+    
+}
+
+bool operator>(const Bignum& a,const Bignum& b)
+{
+    if(a.symbol>b.symbol)
+    return true;
+    else if(a.symbol<b.symbol)
+    return false;
+    else if(a.symbol==1&&b.symbol==1)
+    {
+        int m,n;
+        m=a.a.size();n=b.a.size();
+        if(m>n)
+            return true;
+        else if(m<n)
+            return false;
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(a.a.at(i)!=b.a.at(i))
+                    if(a.a.at(i)>b.a.at(i))
+                        return true;
+                    else
+                        return false;
+                        
+                    
+            }
+        }
+        
+    }
+    else if(a.symbol==0&&b.symbol==0)
+    {
+        int m,n;
+        m=a.a.size();n=b.a.size();
+        if(m>n)
+            return false;
+        else if(m<n)
+            return true;
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(a.a.at(i)!=b.a.at(i))
+                    if(a.a.at(i)>b.a.at(i))
+                        return false;
+                    else
+                        return true;
+                        
+                    
+            }
+        }
+    }
+}
+
+bool operator<(const Bignum& a,const Bignum& b)
+{
+    bool x;
+    x=a>b;
+    if(x==false)
+        return true;
+    else 
+        return false;
+}
+
 int main()
 {
-    
+    bool x;
     Bignum a;
     Bignum b;
     cin>>a>>b;
-    Bignum c(a+b);
+    Bignum c(a+b);Bignum d(a-b);
     cout<<a<<b;
     cout<<c<<endl;
-   
-
+    x=a>b;
+    cout<<x<<endl;
+    x=a<b;
+    cout<<x<<endl;
+    cout<<d<<endl;
     return 0;
 }
